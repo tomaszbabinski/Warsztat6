d79c70b996f88950e174c8c7ba47f62aa03bb35e
tomaszbabinski <tommybab@gmail.com>
2019-03-31 12:39:27 +0200

    Initial set up - persistence.xml pom.xml AppConfig AppInitializer

---
+++ src/main/java/pl/coderslab/warsztat6/app/AppConfig.java
@@ -0,0 +1,56 @@
+package pl.coderslab.warsztat6.app;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.coderslab")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "pl.coderslab.warsztat6.repository")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("warsztat6");
+        return emfb; }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager tm = new JpaTransactionManager(emf);
+        return tm; }
+
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
+    }
+
+    @Bean
+    public org.springframework.validation.Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+}
+
---
+++ src/main/java/pl/coderslab/warsztat6/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pl.coderslab.warsztat6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String renderMainPage(){
+        return "Application starts";
+    }
+}
